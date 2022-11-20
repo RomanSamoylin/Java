45049a05aac2097b34b522fa70c27fb3b30cf574
Roman Samoylin <roman.samoylin@gmail.com>
2022-11-20 20:00:49 +0300

    Lesson1

---
+++ java_projects/program.java
@@ -0,0 +1,26 @@
+package java_projects;
+
+public class program {
+   public static void main (String[]args)  {
+System.out.println("Hello world") ;
+ /** double f = 123.56;*/
+ boolean f = true && false;
+ int i = 123;
+ String s ="qwer"; 
+ s.charAt(1);
+ boolean t = 123 > 255;
+ int a = 5;
+ int b = 2;
+ int[] arr = new int[10];
+ arr[3] = 13;
+ System.out.println(f);
+ System.out.println(i++);
+ System.out.println(i);
+ System.out.println(Integer.MAX_VALUE);
+ System.out.println(s);
+ System.out.println(t);
+ System.out.println(a | b);
+ System.out.println(arr[3]);
+   }
+}
+
