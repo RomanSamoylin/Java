f765e85d8727b38f4a42eedfa7c6df32c48a6b92
Roman Samoylin <roman.samoylin@gmail.com>
2022-12-31 12:32:58 +0300

    Cat

---
+++ OPP_projects/HW2/src/Cat.java
@@ -0,0 +1,25 @@
+package OPP_projects.HW2.src;
+
+public class Cat extends Animal implements Speakable,Runable {
+
+    public Cat(String nickname, int legs, String owner) {
+        super(nickname, legs, owner);
+    }
+
+    @Override
+    public String toString() {
+        return "Cat" + super.toString();
+    }
+
+    @Override
+    public String speak() {
+        return "Cat's voice: Meow";
+    }
+
+    @Override
+    public int runSpeed() {
+        return 50;
+    }
+
+
+}
