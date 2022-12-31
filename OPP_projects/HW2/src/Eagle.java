ebec9be983b1761ac1bdbbb98862bfff4df692bb
Roman Samoylin <roman.samoylin@gmail.com>
2022-12-31 12:38:21 +0300

    Eagle

---
+++ OPP_projects/HW2/src/Eagle.java
@@ -0,0 +1,27 @@
+package OPP_projects.HW2.src;
+
+public class Eagle extends Animal implements Speakable,Runable,Flyable {
+    public Eagle(String nickname, int legs, String owner) {
+        super(nickname, legs, owner);
+    }
+
+    @Override
+    public int flySpeed() {
+        return 100;
+    }
+
+    @Override
+    public int runSpeed() {
+        return 1;
+    }
+
+    @Override
+    public String speak() {
+        return "Eagle's voice: Hhhhhhh";
+    }
+
+    @Override
+    public String toString() {
+        return "Eagle" + super.toString();
+    }
+}
